2fe9ace0bce01a7deedcb23794c51deefd72dcf2
Ross Basarevych <basarevych@gmail.com>
2017-03-18 19:09:55 +0200

    lib update

--- __init__.py
+++ __init__.py
@@ -52,6 +52,24 @@ class Directory(object):
 
         return response['results'][0]
 
+    def unset(self, name):
+        request = {
+            'id': str(uuid.uuid1()),
+            'command': 'unset',
+            'args': [
+                name
+            ]
+        }
+
+        self._socket.send(json.dumps(request))
+        response = json.loads(self._socket.receive())
+
+        if response['id'] != request['id']:
+            raise RuntimeError('Invalid response from daemon')
+
+        if not response['success']:
+            raise RuntimeError('Error: %s' % response['message'])
+
     def wait(self, name, timeout=0):
         request = {
             'id': str(uuid.uuid1()),
